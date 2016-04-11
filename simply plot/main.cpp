92c8c5ea3078432fb61d0121f4436fe4d4da69d0
blackfm <andreydenisov@macbook-pro.local>
2016-04-11 20:54:17 +0200

    Initial Commit

---
+++ simply plot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  simply plot
+//
+//  Created by Andrey Denisov on 11/04/16.
+//  Copyright © 2016 Andrey Denisov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
